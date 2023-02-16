c9757d38b81c345fe1983f70692a84343da6fa69
Livingstone Maghanga <stoneriga@gmail.com>
2023-02-16 16:32:37 +0300

    setup of state managment on index.js

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -10,8 +10,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <div>
+      <HelloWorld msg="Vite + Vue" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  
 </template>
 
 <style scoped>
