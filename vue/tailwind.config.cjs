1300399aec1adbd4c3fbf179217b10a6b78c2e58
Livingstone Maghanga <stoneriga@gmail.com>
2023-02-17 11:20:14 +0300

    bug fix and update on tailwind CSS config

--- vue/tailwind.config.cjs
+++ vue/tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+  plugins: [],
 }
--- vue/vite.config.js
+++ vue/vite.config.js
@@ -3,5 +3,23 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        // treat all tags with a dash as custom elements
+        isCustomElement: (tag) => tag.includes('-')
+      }
+    }
+  })],
+  build: {
+    lib: {
+      entry: './src/entry.js',
+      formats: ['es','cjs'],
+      name: 'web-component',
+      fileName: (format)=>(format === 'es' ? 'index.js' : 'index.cjs')
+    },
+    sourcemap: true,
+    target: 'esnext',
+    minify: false
+  }
 })
